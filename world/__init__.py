2f1ad00ef3e16167edfcb1a628848ff28f7418ca
Tony Kiegel <askiegel@eagles.usi.edu>
2026-07-05 18:17:59 -0500

    Refactor world model into world package

---
+++ world/__init__.py
@@ -0,0 +1,9 @@
+from .model import WorldModel, WorldEntity, EntityObservation
+from .entity_registry import EntityRegistry
+
+__all__ = [
+    "WorldModel",
+    "WorldEntity",
+    "EntityObservation",
+    "EntityRegistry",
+]
